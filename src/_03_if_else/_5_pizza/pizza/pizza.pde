1585112ecf5c494c375297d9227cde92fcfd58f2
league <league@imac-18.attlocal.net>
2022-08-14 11:23:40 -0700

    Done with pizza, partly with riddles

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,25 @@
+PImage pepperoni;
 
 void setup() {
-    
+ size(500, 500);  
+ pepperoni = loadImage("pepperoni.png");
 }
 void draw() {
-    
+   fill(212, 162, 97); 
+   ellipse(250, 250, 475, 475);
+   fill(255, 83, 48);
+   ellipse(250, 250, 450, 450);
+   fill(255, 193, 48);
+   ellipse(250, 250, 440, 440);
+   if (mousePressed){
+   image(pepperoni, 22, 200);
+   image(pepperoni, 97, 237);
+   image(pepperoni, 120.5, 50);
+   image(pepperoni, 287.5, 190.5);
+   image(pepperoni, 37.5, 307.5);
+   image(pepperoni, 287.5, 120.5);
+   image(pepperoni, 307.5, 370.5);
+   image(pepperoni, 237.5, 237.5);
+   
+   }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,16 +2,24 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle = JOptionPane.showInputDialog("What has to be broken before you use it?");
+// 2.  Ask the user a question 
+if(riddle.equals("An egg.")) {
+	JOptionPane.showMessageDialog(null,"Correct");
+	score = score + 1;
+}else {
+	JOptionPane.showMessageDialog(null,"Incorrect, the answer was An egg.");
+}
+JOptionPane.showMessageDialog(null,""+ score);
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
